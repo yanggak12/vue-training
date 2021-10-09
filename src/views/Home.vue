a5e7c53c89cf2cb45ba9308e7c53cdb77ef8c1c0
양윤상 <yanggak12@naver.com>
2021-10-10 07:24:17 +0900

    Vue lifecycle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1>This is Home page</h1>
    <form action="">
      <InputField v-model="name"/>
-     <br><button>submit</button>
+     <br><button @click="update">submit</button>
    </form>
    {{name}}
   </div>
@@ -15,12 +15,38 @@ import InputField from '../components/InputField.vue';
 export default{
   data(){
     return{
-      name:''
+      name:'Yoonsang'
     }
+  },
+  beforeCreate(){
+    console.log('beforeCreate',this.name)
+  },
+  created(){
+    console.log('created',this.name)
+  },
+  beforeMount(){
+    alert('beforeMount')
+  },
+  mounted(){
+    alert('mounted')
+  },
+  beforeUpdate(){
+    console.log('beforeUpdate',this.name)
+  },
+  updated(){
+    alert('updated')
+  },
+  beforeDestroy(){
+    alert('destroyed')
   },
   components:{
     InputField
   },
+  methods:{
+    update(){
+      this.name = 'Hello!'
+    }
+  }
   }
 
 </script>
