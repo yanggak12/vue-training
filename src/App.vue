0d0eb9561729d66c101980a31b643a07bffe3ef7
양윤상 <yanggak12@naver.com>
2021-10-10 11:38:00 +0900

    Todo App - Vue+Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+   <h1 class="text-center">Todo App</h1>
+  <input v-model="todoText" type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo">
+   <hr>
+   <Todo 
+   v-for="todo in todos" 
+   :key="todo.id"
+   :todo="todo"
+   @toggle-checkbox="toggleCheckBox"
+   @click-delete="deleteTodo"
+   />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from './components/Todo.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Todo
+  },
+  data(){
+    return {
+      todoText:'',
+      todos:[
+        {id: 1, text:'ex) Buy a car', checked:false},
+      ]
+    }
+  },
+  methods:{
+    addTodo(){
+      this.todos.push({id:Date.now(),text:this.todoText,checked:false});
+      this.todoText = '';
+    },
+    toggleCheckBox({id,checked}){
+      const index = this.todos.findIndex(todo=>{
+        return todo.id === id;
+        
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id){
+      const index = this.todos.findIndex(todo=>todo.id===id);
+      this.todos = this.todos.filter((el)=>el !== this.todos[index])
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mb-2 d-flex align-items-center">
+     <input 
+     type="checkbox"
+     @change="toggleCheckBox"
+     >
+     <span 
+     class="m-3 flex-grow-1"
+     :class="todo.checked?'text-muted':''"
+     :style="todo.checked?'text-decoration:line-through':''"
+     >
+     {{todo.text}}</span>
+     <button 
+     class="btn btn-danger btn-sm"
+     @click="clickDelete"
+     >
+     Delete</button>
+   </div>
+</template>
+
+<script>
+export default {
+    props:{
+        todo:{
+            type:Object,
+            required:true,
+        }
+    },
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        toggleCheckBox(e){
+            this.$emit('toggle-checkbox',{
+                id:this.todo.id,
+                checked:e.target.checked,
+            })
+        },
+        clickDelete(){
+            this.$emit('click-delete',this.todo.id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
